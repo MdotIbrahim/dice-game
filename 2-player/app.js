450386aa383b87dc689e756733de3c4b81bdcf18
Ibrahim <ibrahim.345xa@slmail.me>
2022-04-28 10:56:06 +0100

    fixed for loop

--- 2-player/app.js
+++ 2-player/app.js
@@ -3,6 +3,7 @@ let score2 = 0;
 let saveScore1 = 0;
 let saveScore2 = 0;
 let holdValue = 1;
+let randomDiceNumber = 0;
 
 let score1InHTML = document.getElementById("score1");
 let score2InHTML = document.getElementById("score2");
@@ -10,18 +11,17 @@ let saveScore1InHTML = document.getElementById("saveScore1");
 let saveScore2InHTML = document.getElementById("saveScore2");
 let player1InHTML = document.getElementById("player1");
 let player2InHTML = document.getElementById("player2");
-let diceClass = document.getElementsByClassName("dice");
+// let diceClass = document.getElementsByClassName("dice");
 let dice1 = document.getElementById("dice1");
 let dice2 = document.getElementById("dice2");
 let dice3 = document.getElementById("dice3");
 let dice4 = document.getElementById("dice4");
 let dice5 = document.getElementById("dice5");
 let dice6 = document.getElementById("dice6");
+let diceList = [dice1, dice2, dice3, dice4 , dice5 , dice6];
 const rollButton = document.getElementById("roll");
 const startButton = document.getElementById("startAgain");
 const holdButton = document.getElementById("hold");
-
-diceList = [dice1, dice2, dice3, dice4 , dice5 , dice6];
 
 const checkDice = (dicenum, score1, score2) => {
     if (score1 > 20) {
@@ -57,8 +57,7 @@ const switchSide = () => {
         player2InHTML.style.color = "red";
         player1InHTML.style.color = "white";
         player2InHTML.textContent = `Player 2's Turn`;
-        player1InHTML.textContent = `Player 1`;
-        
+        player1InHTML.textContent = `Player 1`; 
     }else{
         if (randomDiceNumber != 1){
             saveScore2 = score2;
@@ -67,27 +66,20 @@ const switchSide = () => {
             // score2 = 0 //error
             score2 = saveScore2
         }
-        
         saveScore2InHTML.textContent = `Current Reset: ${saveScore2}`
         holdValue = 1
         player1InHTML.style.color = "red";
         player2InHTML.style.color = "white";
         player1InHTML.textContent = `Player 1's Turn`;
         player2InHTML.textContent = `Player 2`;
-        
     }
 }
 
-let randomDiceNumber = 0;
 rollButton.addEventListener("click", () => {
     randomDiceNumber = Math.floor((Math.random()* 6) + 1);
-
-    dice1.style.display = "none";
-    dice2.style.display = "none";
-    dice3.style.display = "none";
-    dice4.style.display = "none";
-    dice5.style.display = "none";
-    dice6.style.display = "none";
+    for (let i = 0; i < diceList.length; i++) {
+        diceList[i].style.display = "none"
+    }
     if (holdValue == 1){
         diceList[randomDiceNumber - 1].style.display="block";
         score1 += randomDiceNumber;
@@ -121,18 +113,17 @@ startButton.addEventListener("click", () => {
     player1InHTML.textContent = `Player 1's Turn`;
     player2InHTML.style.color = "white";
     player2InHTML.textContent = `Player 2`;
-    dice1.style.display = "none";
-    dice2.style.display = "none";
-    dice3.style.display = "none";
-    dice4.style.display = "none";
-    dice5.style.display = "none";
-    dice6.style.display = "none";
+    for (let i = 0; i < diceList.length; i++) {
+        diceList[i].style.display = "none"
+    }
+    // for (let dice in diceList) {
+    //     dice.style.display = "none"
+    // }
     // startButton.style.display = "none";
 })
 
 holdButton.addEventListener("click", () => {
     switchSide()
 })
-
 
 
